4d028dbd9a3d1669d4d240ecc26770d2eb8959d8
YTCodingClass <familiekamhieh@gmail.com>
2021-09-08 22:46:54 +0200

    Add project files.

---
+++ Radiobutton/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Radiobutton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string chosenColor;
+        private string chosenGender;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = $"Your chosen Color is: {chosenColor}\nYour chosen gender is: {chosenGender}";
+            MessageBox.Show(text);
+
+        }
+
+        private void ColorsRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            chosenColor = (sender as RadioButton).Content.ToString();
+        }
+
+        private void GenderRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            chosenGender = (sender as RadioButton).Content.ToString();
+        }
+    }
+}
